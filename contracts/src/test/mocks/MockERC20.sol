b74f615602f49172ed9caf725a011e9dc0ebbcff
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-08-04 17:15:58 +0200

    ADD: ERC20 token

---
+++ contracts/src/test/mocks/MockERC20.sol
@@ -0,0 +1,15 @@
+// contracts/test/mocks/MockERC20.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock Token", "MTK") {
+        _mint(msg.sender, 1_000_000 * 10**18);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
