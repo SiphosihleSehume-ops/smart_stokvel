4a5b35e2afcf0791f720c444af171f44cecaea43
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-09-21 10:48:16 +0200

    ADD: additional imports

--- contracts/test/UserRegistry.t.sol
+++ contracts/test/UserRegistry.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../UserRegistry.sol";
+import "src/UserRegistry.sol";
 
 contract UserRegistryTest is Test {
     UserRegistry public registry;
